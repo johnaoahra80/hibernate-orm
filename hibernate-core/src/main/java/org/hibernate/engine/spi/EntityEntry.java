f9a9ec1a444652220893718685226199009c7f85
John O'Hara <johara@localhost.localdomain>
2015-03-19 10:19:18 +0000

    Reinstated EnityEntry file header

--- hibernate-core/src/main/java/org/hibernate/engine/spi/EntityEntry.java
+++ hibernate-core/src/main/java/org/hibernate/engine/spi/EntityEntry.java
@@ -1,3 +1,26 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * Copyright (c) 2010-2014, Red Hat Inc. or third-party contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors.  All third-party contributions are
+ * distributed under license by Red Hat Inc.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA  02110-1301  USA
+ */
 package org.hibernate.engine.spi;
 
 import org.hibernate.LockMode;
@@ -8,7 +31,15 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /**
- * Created by johara on 13/03/15.
+ * We need an entry to tell us all about the current state of an object with respect to its persistent state
+ *
+ * Implementation Warning: Hibernate needs to instantiate a high amount of instances of this class,
+ * therefore we need to take care of its impact on memory consumption.
+ *
+ * @author Gavin King
+ * @author Emmanuel Bernard <emmanuel@hibernate.org>
+ * @author Gunnar Morling
+ * @author Sanne Grinovero  <sanne@hibernate.org>
  */
 public interface EntityEntry {
 	LockMode getLockMode();
